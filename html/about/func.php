31283a9142539ae43b8f3384109e8057993f6ada
bcurran07 <brendanmcurran@yahoo.com>
2011-11-07 11:37:23 -0800

    making Federica design updates

---
+++ html/about/func.php
@@ -0,0 +1,51 @@
+<?
+
+function printheader() {
+	echo "<div id='header'>
+	<div id='mainlogo' name='GameGami'></div><div align='right' id='tagline'>Mobile Games Made Social</div></div>
+	<div id='navbar'>
+		<div align='center'>
+		<a href='http://www.gamegami.com'>Home</a>
+	    <a href='http://www.gamegami.com/blog'>Blog</a>
+	    <a href='http://www.gamegami.com/api'>API</a>
+	    <a href='http://www.gamegami.com/about'>About us</a>
+	    <a href='http://www.gamegami.com/contact'>Contact</a>
+		</div>
+	</div>";
+	
+}
+
+function printfooter() {
+	echo "<div id='footer' align='center'>
+	    <p class='links'>
+	    <div id='social'>
+	<div id='fb-root'></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) {return;}
+	  js = d.createElement(s); js.id = id;
+	  js.src = '//connect.facebook.net/en_US/all.js#xfbml=1';
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+	<br /><br />
+	<a href='https://twitter.com/share' class='twitter-share-button'>Tweet</a>
+	    <!-- Place this tag where you want the +1 button to render -->
+	    <g:plusone annotation='inline' width='200'></g:plusone>
+	<div class='fb-like' data-href='www.gamegami.com' data-send='false' data-width='350' data-show-faces='false' data-action='recommend' data-colorscheme='dark'></div>  
+	    </div>
+	    </p>
+	</div>
+	</div>
+	</div>
+	    <script type='text/javascript'>
+	      (function() {
+	         var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+	         po.src = 'https://apis.google.com/js/plusone.js';
+	         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	      })();
+	    </script>";
+	
+}
+
+?>
---
+++ html/about/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<? include 'func.php'?>
+ <link rel="stylesheet" type="text/css" href="http://www.gamegami.com/gamegami.css">
+<meta charset="utf-8" />
+<title>GameGami :: About</title>
+<script src="//platform.twitter.com/widgets.js" type="text/javascript"></script>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+</head>
+
+<body>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-26235804-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<div id='container'>
+<div align='center'><div id='pagecontent'>
+<?
+	printheader();
+?>
+
+<div  align='center'><div class='about_main'>
+	<br />
+	<h1 style="margin-top: 50px; font-size: 32px;">About Us</h1>
+<div id='aboutus' align='justify'>We are a team of passionate gamers from UC Berkeley. We placed 2nd in 2011 in the UC Berkeley Business Plan competition and we believe that we can multiply the fun by inviting or challenging our friends on our favorite mobile games.</div>
+<div alt="Inaki's page" align="left" id="InakiDiv">
+<div id='InakiImg'></div>	
+<div align='justify' id='InakiText'><strong>Iñaki Ruiz</strong> holds a Business Administration degree and an MBA from UC Berkeley. He has 12 years of professional experience in Strategic Consulting and the Telecom industry, where he has been Marketing Director for a $500M revenue Business Unit.</div>
+</div>
+<div id="InakiLine"></div>
+<div alt="Ismael's page" align="left" id="IsmaelDiv">
+<div id='IsmaelImg'></div>	
+<div align='justify' id='IsmaelText'><strong>Ismael Ghozael</strong> holds an MSc in Electrical Engineering from Grenoble, France and an MBA from UC Berkeley. He has 6 years of Product Management experience in the mobile phone industry where he created phones for luxury brands and other mass market phones).</div>
+</div>
+<div id="IsmaelLine"></div>
+<div alt="Brendan's page" align="left" id="BrendanDiv">
+<div id='BrendanImg'></div>	
+<div align='justify' id='BrendanText'><strong>Brendan Curran</strong> holds an MS in Information Management and Systems from UC Berkeley. Before returning to school, Brendan contributed his project management and user experience skills as an independent consultant with Microsoft and Google. He has also worked with global development nonprofits like <a href="http://www.kiva.org">Kiva</a> and <a href="http://www.seeyourimpact.org">SeeYourImpact.</div>
+</div>
+</div></div>
+</div>
+</div>
+<? printfooter(); ?>
+</body>
+</html>
